8260f189d2fd57abc501d9c8bebecf2d18bfcaee
angelachan64 <angelachan64@gmail.com>
2015-02-08 21:25:55 -0500

    Recursion

--- 01-Recursion/Driver.java
+++ 01-Recursion/Driver.java
@@ -2,17 +2,26 @@ import java.util.*;
 
 public class Driver{
     public static void main(String args[]){
-	int test;
+	int test,bigtest;
 	String string = "asdaaaaasdw";
 	Driver d = new Driver();
 	Random r = new Random();
 	test = r.nextInt(10);
+	bigtest = r.nextInt(1000);
 	System.out.println("Test value is: " + test);
-	System.out.println("Fib(test) is: " + d.fib(test));
+	System.out.println("fib(test) is: " + d.fib(test));
 	//System.out.println("Length of asdfw is: " + d.len(string));
 	System.out.println("factorial(test) is: " + d.factorial(test));
-	System.out.println("count(asdfwioeasdfw, a) is: " + d.count(string, 'a'));
 	System.out.println("fibonacci(test) is: " + d.fibonacci(test));
+	System.out.println("bunnyEars2(test) is: " + d.bunnyEars2(test));
+	System.out.println();
+	System.out.println("String is: " + string);
+	System.out.println("count(string, a) is: " + d.count(string, 'a'));
+	System.out.println("strCount(string,a) is: " + d.strCount(string, "a"));
+	System.out.println("allStar(string) is: " + d.allStar(string));
+	System.out.println();
+	System.out.println("Bigtest value is: " + bigtest);
+	System.out.println("sumDigits(bigtest) is: " + d.sumDigits(bigtest));
     }
     public int factorial(int n){
 	if(n == 0){
@@ -58,6 +67,34 @@ public class Driver{
     public int bunnyEars2(int bunnies){
 	if (bunnies <= 0){
 	    return 0;
+	} else if (bunnies%2 == 0){
+	    return 3 + bunnyEars2(bunnies-1);
+	} else{
+	    return 2 + bunnyEars2(bunnies-1);
+	}
+    }
+    public int strCount(String str, String sub){
+	int length = sub.length();
+	if (str.length() < length){
+	    return 0;
+	} else if(str.substring(0,length).equals(sub)){
+	    return 1 + strCount(str.substring(1), sub);
+	} else{
+	    return strCount(str.substring(1), sub);
+	}
+    }
+    public int sumDigits(int n){
+	if (n/10==0){
+	    return n;
+	} else{
+	    return n%10 + sumDigits(n/10);
+	}
+    }
+    public String allStar(String str){
+	if (str.length() == 1){
+	    return str;
+	} else{
+	    return str.substring(0,1) + "*" + allStar(str.substring(1));
 	}
     }
 }
