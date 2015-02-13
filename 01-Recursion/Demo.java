2e30ff96cb044925fcd42adf726f8d2c0dcdd048
angelachan64 <angelachan64@gmail.com>
2015-02-13 08:15:24 -0500

    some printf stuff

---
+++ 01-Recursion/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String[] args){
+	System.out.printf("Hello");
+	System.out.printf(" World!\n");
+	System.out.printf("before %s after\n","middle");
+	System.out.printf("before %s after %s stuff","middle","more");
+	System.out.printf("An int: %d\n",123);
+	System.out.printf("An int: %6d A double: %6.2f\n",123,123.0);
+    }
+}
