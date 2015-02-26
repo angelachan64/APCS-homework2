048417aa5d605c732eee0e043fd20d04eb2075a7
angelachan64 <angelachan64@gmail.com>
2015-02-26 08:42:07 -0500

    timing

---
+++ 04-MergeSort/README.md
@@ -0,0 +1,17 @@
+<b><i>For 20 items in an array:</b></i><br>
+___
+<b>Selection Sort:</b><br>
+30 Comparisons<br>
+0.123 seconds<br>
+<b>Insertion Sort:</b><br>
+100 Comparisons<br>
+0.100 seconds<br>
+<b>Bubble Sort:</b><br>
+105 Comparisons<br>
+0.110 seconds<br>
+<b>Merge Sort:</b><br>
+18 Comparisons<br>
+0.061 seconds<br>
+___
+<b><i>For 1000 items in an array:</b></i><br>
+___
--- 04-MergeSort/mergesort.java
+++ 04-MergeSort/mergesort.java
@@ -7,16 +7,27 @@ public class mergesort{
 
     public static void main(String[] args){
 	mergesort ms = new mergesort();
-	System.out.println(ms.A);
-	System.out.println(ms.B);
-	System.out.println("Merged: " + ms.merge(ms.A,ms.B));
+	//System.out.println(ms.A);
+	//System.out.println(ms.B);
+	//System.out.println("Merged: " + ms.merge(ms.A,ms.B));
+	ms.merge(ms.A,ms.B);
     }
     public mergesort(){
-	A.add(1);A.add(3);A.add(5);A.add(7);A.add(22);
-	B.add(2);B.add(3);B.add(7);B.add(10);B.add(25);
+	Random r = new Random();
+	//A.add(1);A.add(3);A.add(5);A.add(7);A.add(22);
+	//B.add(2);B.add(3);B.add(7);B.add(10);B.add(25);
+	for (int i=0;i<10;i++){
+	    if (i==0){
+		A.add(r.nextInt(10)); B.add(r.nextInt(10));
+	    } else{
+		A.add(r.nextInt(10) + A.get(i-1));
+		B.add(r.nextInt(10) + B.get(i-1));
+	    }
+	}
     }
     public ArrayList<Integer> merge(ArrayList<Integer> A, ArrayList<Integer> B){
 	ArrayList<Integer> ai = new ArrayList<Integer>();
+	int count=0,add=0;
 	while (A.size()>0 && B.size()>0){
 	    if (A.get(0) <= B.get(0)){
 		ai.add(A.get(0));
@@ -24,17 +35,20 @@ public class mergesort{
 	    } else{
 		ai.add(B.get(0));
 		B.remove(0);
-	    } 
+	    } count++;
 	} if (A.size()>0){
 	    while (A.size()>0){
 		ai.add(A.get(0));
 		A.remove(0);
+		add++;
 	    }
 	} else if (B.size()>0){
 	    while (B.size()>0){
 		ai.add(B.get(0));
 		B.remove(0);
+		add++;
 	    }
-	} return ai;
+	} System.out.println(count + " comparisons");
+	return ai;
     }
 }
