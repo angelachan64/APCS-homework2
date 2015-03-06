506fa7199efbfa83718489f93131016b9a0efb8f
angelachan64 <angelachan64@gmail.com>
2015-03-05 20:50:30 -0500

    Partition

--- 05-Selection/Select.java
+++ 05-Selection/Select.java
@@ -16,14 +16,34 @@ public class Select{
 
     public static void main(String[] args){
 	Select s = new Select(10);
+	System.out.println(s.list);
+	s.selection(s.list);
     }
     public Select(int n){
-	Random r = new Random()
+	Random r = new Random();
+	int num;
 	for (int i=0;i<n;i++){
-		list.add(r.nextInt(100));
+	    num = r.nextInt(100);
+	    if (list.contains(num)){
+		i--;
+	    } else{
+		list.add(num);
+	    }
 	}
     }
     public int selection(ArrayList<Integer> A){
-
+	Random r = new Random();
+	//ArrayList<Integer> al = new ArrayList<Integer>();
+	int ind = r.nextInt(A.size());
+	int num = A.get(ind);
+	A.add(0, A.remove(ind));
+	for (int i=1;i<A.size();i++){
+	    if (A.get(i)<num){
+		A.add(0,A.remove(i));
+	    }
+	}
+	System.out.println(num);
+	System.out.println(A);
+	return 0;
     }
 }
