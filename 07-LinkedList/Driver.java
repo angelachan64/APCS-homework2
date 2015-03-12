26827780eb369ff75ba9667134cb7f863abd734e
angelachan64 <angelachan64@gmail.com>
2015-03-12 08:21:35 -0400

    looping through a linked list

--- 07-LinkedList/Driver.java
+++ 07-LinkedList/Driver.java
@@ -1,24 +1,24 @@
 public class Driver{
     public static void main(String[] args){
 	Node n = new Node("Hello");
-	System.out.println(n);
+	//System.out.println(n);
 
 	Node n2 = new Node("World");
-	System.out.println(n2);
+	//System.out.println(n2);
 
 	n.setNext(n2);
 
-	System.out.println(n.getNext().getData());
-	System.out.println(n.getNext());
+	//System.out.println(n.getNext().getData());
+	//System.out.println(n.getNext());
 
 	n.getNext().setData("pickle");
-	System.out.println(n2);
+	//System.out.println(n2);
 
 	n2.setData("watermelon");
-	System.out.println(n.getNext().getData());
+	//System.out.println(n.getNext().getData());
 
 	n2.setNext(new Node("abc"));
-	System.out.println(n.getNext().getNext().getData());
+	//System.out.println(n.getNext().getNext().getData());
 
 	n.getNext().getNext().setNext(new Node("yabba dabba doo"));
 
@@ -26,8 +26,18 @@ public class Driver{
 	tmp.setNext(n);
 	n = tmp;
 
-	System.out.println(tmp);
-	System.out.println(n);
-	System.out.println(n.getNext());
+	//System.out.println(tmp);
+	//System.out.println(n);
+	//System.out.println(n.getNext());
+
+	tmp = n;
+	while (tmp != null){
+	    System.out.println(tmp);
+	    tmp = tmp.getNext();
+	}
+	System.out.println();
+	for (tmp = n;tmp != null;tmp = tmp.getNext()){
+	    System.out.println(tmp);
+	}
     }
 }
