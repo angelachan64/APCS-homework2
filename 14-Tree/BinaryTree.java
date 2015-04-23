0d4682f8e8bd2ee49e80e64ea302c59cc90d22f1
angelachan64 <angelachan64@gmail.com>
2015-04-23 08:41:56 -0400

    Tree

---
+++ 14-Tree/BinaryTree.java
@@ -0,0 +1,19 @@
+public class BinaryTree{
+    private Node root;
+
+    public BinaryTree(){
+	Node tmp = new Node();
+	root = tmp;
+    }
+    public BinaryTree(Node n){
+	root = n;
+    }
+
+    insert(){
+
+    }
+
+    public static void main(String[] args){
+	BinaryTree tree = new BinaryTree(new Node());
+    }
+}
